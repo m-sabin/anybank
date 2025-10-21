9bd2e4eba32c2294c74b5e07776c6b3f4335d162
Missagne <scriptnibas@gmail.com>
2025-10-21 15:24:58 -0300

    tornando a class Conta em abstract class para ela nao ser intanciada

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,22 +1,8 @@
 import '../lib/conta.dart';
 
 void main() {
-  Conta contaMatheus = Conta("Matheus", 1000);
-  Conta contaLuiza = Conta("Luiza", 2000);
   ContaCorrente contaChris = ContaCorrente("Chris", 4000);
   ContaPoupanca contaRoberta = ContaPoupanca("Roberta", 4000);
-
-  List<Conta> contas = <Conta>[contaMatheus, contaLuiza];
-
-  for (Conta conta in contas) {
-    conta.imprimeSoldo();
-  }
-
-  contaLuiza.receber(3000);
-
-  contaMatheus.receber(500);
-
-  contaLuiza.enviar(200);
 
   contaRoberta.imprimeSoldo();
   contaRoberta.enviar(4300);
